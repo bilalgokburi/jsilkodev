ffec45e5db10639ecd46d2507370c1cef0cd1a51
Bilal Gokburi <gkburibilall2@gmail.com>
2022-10-17 22:41:10 +0300

    odev

---
+++ js/clock.js
@@ -0,0 +1,28 @@
+
+// promt start
+let userName = prompt("Adınızı Giriniz")
+let info = document.querySelector("#name")
+
+info.innerHTML = `<strong>${userName}</strong>`
+// promt end
+
+function showTime() {
+    const today = new Date();
+    let h = today.getHours();
+    let m = today.getMinutes();
+    let s = today.getSeconds();
+    m = checkTime(m);
+    s = checkTime(s);
+    document.getElementById('clock').innerHTML =  h + ":" + m + ":" + s;
+    setTimeout(showTime, 1000);
+  }
+  
+  function checkTime(i) {
+    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+    return i;
+  }
+  showTime()
+
+
+
+
